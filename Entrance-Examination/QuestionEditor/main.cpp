28972dcbc3e44f87ba7c491183a72456bef75c64
Bibek Dahal <bibekdahal_bd16@hotmail.com>
2014-06-30 18:09:45 +0545

    few changes

--- Entrance-Examination/QuestionEditor/main.cpp
+++ Entrance-Examination/QuestionEditor/main.cpp
@@ -12,6 +12,7 @@ using namespace std;
 
 HWND g_main;
 #pragma comment( lib, "comctl32.lib" )
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' " "version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 HWND g_hdlg;
 
